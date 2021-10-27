be4ec454e848b6ae2676931175d55f67099cbd7c
c4jc <59806195+c4jc@users.noreply.github.com>
2021-10-27 19:53:30 +0200

    create news section

---
+++ src/components/templates/NewsSection/NewsSection.js
@@ -0,0 +1,70 @@
+import React, { useState, useEffect } from 'react';
+import { ArticleWrapper, ContentWrapper, NewsSectionHeader, TitleWrapper, Wrapper } from 'components/templates/NewsSection/NewsSection.styles';
+import { Button } from 'components/atoms/Button/Button';
+import axios from 'axios';
+
+const REACT_APP_DATOCMS_TOKEN = '65b126280309d311d924177f988785';
+export const query = `
+         {
+          allArticles {
+            id
+            title
+            category
+            content
+            image {
+              url
+            }
+          }
+        }
+      `;
+
+const NewsSection = () => {
+  const [articles, setArticles] = useState([]);
+  const [error, setError] = useState('');
+
+  useEffect(() => {
+    axios
+      .post(
+        'https://graphql.datocms.com/',
+        {
+          query,
+        },
+        {
+          headers: {
+            authorization: `Bearer ${REACT_APP_DATOCMS_TOKEN}`,
+          },
+        }
+      )
+      .then(({ data: { data } }) => {
+        setArticles(data.allArticles);
+      })
+      .catch(() => {
+        setError(`Sorry, we couldn't load articles for you`);
+      });
+  }, []);
+
+  return (
+    <Wrapper>
+      <NewsSectionHeader>University news feed</NewsSectionHeader>
+      {articles.length > 0 ? (
+        articles.map(({ id, title, category, content, image = null }) => (
+          <ArticleWrapper key={id}>
+            <TitleWrapper>
+              <h3>{title}</h3>
+              <p>{category}</p>
+            </TitleWrapper>
+            <ContentWrapper>
+              <p>{content}</p>
+              {image ? <img src={image.url} alt="article" /> : null}
+            </ContentWrapper>
+            <Button isBig>Read more</Button>
+          </ArticleWrapper>
+        ))
+      ) : (
+        <NewsSectionHeader>{error ? error : 'Loading...'}</NewsSectionHeader>
+      )}
+    </Wrapper>
+  );
+};
+
+export default NewsSection;
