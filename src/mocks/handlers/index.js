b9f90ae6e4cbe3d62b48184ed6c57cdcb22cbb88
c4jc <59806195+c4jc@users.noreply.github.com>
2021-11-11 18:32:13 +0100

    downbar

--- src/mocks/handlers/index.js
+++ src/mocks/handlers/index.js
@@ -1,14 +1,21 @@
 import { rest } from 'msw';
 import { students } from 'mocks/data/students';
 import { groups } from 'mocks/data/groups';
+import { db } from 'mocks/db';
 
 export const handlers = [
   rest.get('/groups', (req, res, ctx) => {
     return res(ctx.status(200), ctx.json({ groups }));
   }),
-  rest.get('/students/:group', (req, res, ctx) => {
-    if (req.params.group) {
-      const matchingStudents = students.filter((student) => student.group === req.params.group);
+  rest.get('/groups/:id', (req, res, ctx) => {
+    if (req.params.id) {
+      const matchingStudents = db.student.findMany({
+        where: {
+          group: {
+            equals: req.params.id,
+          },
+        },
+      });
       return res(
         ctx.status(200),
         ctx.json({
@@ -21,6 +28,49 @@ export const handlers = [
       ctx.status(200),
       ctx.json({
         students,
+      })
+    );
+  }),
+  rest.get('/students/:id', (req, res, ctx) => {
+    if (req.params.id) {
+      const matchingStudent = db.student.findFirst({
+        where: {
+          id: {
+            equals: req.params.id,
+          },
+        },
+      });
+      if (!matchingStudent) {
+        return res(
+          ctx.status(404),
+          ctx.json({
+            error: 'No matching student',
+          })
+        );
+      }
+      return res(
+        ctx.status(200),
+        ctx.json({
+          students: matchingStudent,
+        })
+      );
+    }
+
+    return res(
+      ctx.status(200),
+      ctx.json({
+        students,
+      })
+    );
+  }),
+  rest.post('/students/search', (req, res, ctx) => {
+    const matchingStudents = req.body.searchPhrase
+      ? students.filter((student) => student.name.toLowerCase().includes(req.body.searchPhrase.toLowerCase()))
+      : [];
+    return res(
+      ctx.status(200),
+      ctx.json({
+        students: matchingStudents,
       })
     );
   }),
--- src/views/Dashboard.js
+++ src/views/Dashboard.js
@@ -1,39 +1,55 @@
 import React, { useEffect, useState } from 'react';
-import axios from 'axios';
-import { ViewWrapper } from 'components/molecules/ViewWrapper/ViewWrapper';
-import { Link, useParams } from 'react-router-dom';
-import UsersList from 'components/organisms/UsersList/UsersList';
+import { Redirect, useParams } from 'react-router-dom';
+import { Link } from 'react-router-dom';
+import StudentsList from 'components/organisms/StudentsList/StudentsList';
+import { useStudents } from 'hooks/useStudents';
+import { GroupWrapper, TitleWrapper, Wrapper } from 'views/Dashboard.styles';
+import { Title } from 'components/atoms/Title/Title';
+import useModal from 'components/organisms/Modal/useModal';
+import StudentDetails from 'components/molecules/StudentDetails/StudentDetails';
+import Modal from 'components/organisms/Modal/Modal';
 
 const Dashboard = () => {
-  const [students, setStudents] = useState([]);
   const [groups, setGroups] = useState([]);
+  const [currentStudent, setCurrentStudent] = useState(null);
+  const { getGroups, getStudentById } = useStudents();
   const { id } = useParams();
+  const { isOpen, handleOpenModal, handleCloseModal } = useModal();
 
   useEffect(() => {
-    axios
-      .get('/groups')
-      .then(({ data }) => setGroups(data.groups))
-      .catch((err) => console.log(err));
-  }, []);
+    (async () => {
+      const groups = await getGroups();
+      setGroups(groups);
+    })();
+  }, [getGroups]);
 
-  useEffect(() => {
-    axios
-      .get(`/students/${id || groups[0]}`)
-      .then(({ data }) => setStudents(data.students))
-      .catch((err) => console.log(err));
-  }, [id, groups]);
+  const handleOpenStudentDetails = async (id) => {
+    const student = await getStudentById(id);
+    setCurrentStudent(student);
+    handleOpenModal();
+  };
+
+  if (!id && groups.length > 0) return <Redirect to={`/group/${groups[0]}`} />;
 
   return (
-    <ViewWrapper>
-      <nav>
-        {groups.map((group) => (
-          <Link key={group} to={`/group/${group}`}>
-            {group}{' '}
-          </Link>
-        ))}
-      </nav>
-      <UsersList users={students} />
-    </ViewWrapper>
+    <Wrapper>
+      <TitleWrapper>
+        <Title as="h2">Group {id}</Title>
+        <nav>
+          {groups.map((group) => (
+            <Link key={group} to={`/group/${group}`}>
+              {group}{' '}
+            </Link>
+          ))}
+        </nav>
+      </TitleWrapper>
+      <GroupWrapper>
+        <StudentsList handleOpenStudentDetails={handleOpenStudentDetails} />
+        <Modal isOpen={isOpen} handleClose={handleCloseModal}>
+          <StudentDetails student={currentStudent} />
+        </Modal>
+      </GroupWrapper>
+    </Wrapper>
   );
 };
 
